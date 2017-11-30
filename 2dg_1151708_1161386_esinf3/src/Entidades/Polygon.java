5652379e3411bcbaf0e0d7080d9ae8c441adc0fa
Miguel Santos [1161386] <1161386@isep.ipp.pt>
2017-11-30 17:59:31 +0000

    Classe Polygon, ServicoPoligonos e Leitura Ficheiro

---
+++ 2dg_1151708_1161386_esinf3/src/Entidades/Polygon.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.util.Objects;
+
+public class Polygon implements Comparable<Polygon> {
+
+    private String nome;
+    private int numeroLados;
+
+    public Polygon(String nome, int numeroLados) {
+        this.nome = nome;
+        this.numeroLados = numeroLados;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + numeroLados;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 11 * hash + Objects.hashCode(this.nome);
+        hash = 11 * hash + this.numeroLados;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Polygon other = (Polygon) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (this.numeroLados != other.numeroLados) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int compareTo(Polygon o) {
+        if (numeroLados > o.numeroLados) {
+            return 1;
+        } else if (numeroLados < o.numeroLados) {
+            return -1;
+        }
+        
+        return 0;    
+    }
+
+}
---
+++ 2dg_1151708_1161386_esinf3/src/Utilitarios/LeituraFicheiros.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilitarios;
+
+import Entidades.Polygon;
+import PL.AVL;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class LeituraFicheiros {
+    private ArrayList<String> lerFicheiro(String nomeFicheiro) {
+        ArrayList<String> linhas = new ArrayList<>();
+        Scanner read = null;
+        try {
+            read = new Scanner(new java.io.FileReader(nomeFicheiro));
+            while (read.hasNext()) {
+                String next = read.next();
+                linhas.add(next);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Ficheiro nao encontrado");
+        } finally {
+            if (read != null) {
+                read.close();
+            }
+        }
+        return linhas;
+    }
+
+    /**
+     *
+     */
+    public void construcaoArvore(AVL<Polygon> arvore, ArrayList<String> list){ 
+        for (String linha : list) {
+            String[] campos = linha.split(";");
+            Polygon p = new Polygon(campos[0], Integer.parseInt(campos[1]));
+            
+            arvore.insert(p);
+        }   
+    }
+    
+}
+
+
